8ab44d7ce2d62e6f22ef9e78c832aa1cbc5164fa
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-08-29 20:02:42 -0700

    add basic fetch function

--- backend/routes/api/igdb.js
+++ backend/routes/api/igdb.js
@@ -1,57 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 const { body } = require("express-validator");
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const response = await axios.post('https://api.igdb.com/v4/games',
-        {
+        const response = await axios("https://api.igdb.com/v4/games", {
+            method: "POST",
             headers: {
-                // 'Accept': 'application/json',
-                'Client-ID': process.env.CLIENT_ID,
-                'Authorization': `Bearer ${process.env.AUTH_TOKEN}`
+                Accept: "application/json",
+                "Client-ID": process.env.CLIENT_ID,
+                Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
             },
-        }
-        );
+            data: 'limit 20;'
+        });
         return res.json(response.data);
-        
     } catch (error) {
         console.error(error);
         if (error.response) {
-    
-            return res.status(error.response.status).json({ error: 'Failed to fetch data' });
+            return res
+                .status(error.response.status)
+                .json({ error: "Failed to fetch data" });
         } else {
-    
-            return res.status(500).json({ error: 'Internal Server Error' });
+            return res.status(500).json({ error: "Internal Server Error" });
         }
     }
 });
 
 router.get("/:id", async (req, res) => {
     try {
-        const response = await axios('https://api.igdb.com/v4/games', 
-            {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Client-ID': process.env.CLIENT_ID,
-                    'Authorization': `Bearer ${process.env.AUTH_TOKEN}`
-                },
-                data: `fields name, cover.url, genres.name, summary, screenshots.url, videos.video_id; where id = ${req.params.id};`
+        const response = await axios("https://api.igdb.com/v4/games", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Client-ID": process.env.CLIENT_ID,
+                Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
             },
-        );
+            data: `fields name,summary,genres,first_release_date,involved_companies,cover.url,screenshots.url,videos; where id = ${req.params.id};`,
+        });
         // debugger
 
         return res.json(response.data);
-
     } catch (error) {
         console.error(error);
         if (error.response) {
-
-            return res.status(error.response.status).json({ error: 'Failed to fetch data' });
+            return res
+                .status(error.response.status)
+                .json({ error: "Failed to fetch data" });
         } else {
-            return res.status(500).json({ error: 'Internal Server Error' });
+            return res.status(500).json({ error: "Internal Server Error" });
         }
     }
 });
