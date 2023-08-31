18a461d4ec9b38a60e0707d4a20deb9fdd9ebfbf
Weyman Leung <leungweyman@gmail.com>
2023-08-31 00:53:11 -0700

    add profile page skeleton

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import GameListItem from '../GameListItem/GameListItem';
 import './Profile.css';
 
 function Profile() {
@@ -9,6 +10,36 @@ function Profile() {
   return (
     <>
       <h2>Welcome, {currentUser.username}</h2>
+	  <div className='profile-page-container'>
+		<div className='profile-page-lists'> 
+			<h3>Wishlist</h3>
+			<div className='profile-page-wishlist'>
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+			</div>
+
+			<h3>Library</h3>
+			<div className='profile-page-library'> 
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+				<GameListItem />
+			</div>
+		</div>
+		<div className='profile-page-friends'>
+			<h3>Friends</h3>
+			<div>Friend 1</div>
+			<div>Friend 2</div>
+			<div>Friend 3</div>
+			<div>Friend 4</div>
+		</div>
+	  </div>
     </>
   );
 }
