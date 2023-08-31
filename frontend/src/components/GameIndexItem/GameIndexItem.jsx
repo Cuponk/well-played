6bbb2176b06417557a7eba826111fcef053f0a9d
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-08-31 14:42:22 -0700

    add basic serach

--- frontend/src/components/GameIndexItem/GameIndexItem.jsx
+++ frontend/src/components/GameIndexItem/GameIndexItem.jsx
@@ -1,10 +1,21 @@
-import "./GameIndexItem.css"
+import "./GameIndexItem.css";
+import defaultImage from "../../assets/images/drawing.jpg";
 
-function GameIndexItem({ game }) {
+function GameIndexItem({ game, id }) {
+
+  const parseImages = (url, type) => {
+    if (!url) {
+      return defaultImage;
+    }
+    return url.replace("t_thumb", type);
+}
+
   return (
-    <>
-      <h1>Game: {game.title} </h1>
-    </>
+    <li className="game-item">
+      <p>{game.name}</p>
+      <img src={parseImages(game.cover?.url, 't_cover_small')} className="index-cover" />
+      <p>{game.genres[0].name ? game.genres[0].name : ''}</p>
+    </li>
   )
 }
 
