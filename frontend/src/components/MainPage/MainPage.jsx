8aee907318b5c274128620ddf06d8faef56ad1f7
Julio Uribe <uribejulioc@gmail.com>
2023-09-01 09:54:49 -0700

    Add links for search and profile plus footer text

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import "./MainPage.css";
 import wellPlayed from "../../assets/images/Well-Played.svg";
+import { NavLink } from "react-router-dom/";
 
 function MainPage() {
   return (
     <>
       <div className="main-page-container">
         <img src={wellPlayed} alt="Well Played splash"></img>
+        <NavLink to="/search"><button>Search Games</button></NavLink>
       </div>
     </>
   );
