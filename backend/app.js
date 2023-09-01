4785effc8d7aa98eba604327944306cbab20b6d9
Julio Uribe <uribejulioc@gmail.com>
2023-09-01 10:10:22 -0700

    Add setup code so Render can run the web app

--- backend/app.js
+++ backend/app.js
@@ -56,13 +56,33 @@ app.use(
 // Express Routers
 app.use('/api/igdb', dbRouter);
 app.use('/api/users', usersRouter);
-
 app.use('/api/reviews', reviewsRouter);
-
 app.use('/api/customGames', customGamesRouter);
-
 app.use('/api/csrf', csrfRouter);
 app.use('/api/friendships', friendshipsRouter);
+
+// Serve static React build files statically in production
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+}
 
 // Express custom middleware for catching all unmatched requests and formatting
 // a 404 error to be sent as the response.
