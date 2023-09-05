a57b3a1f39f90d52141f6e5e1f9929c5db8aec58
Weyman Leung <leungweyman@gmail.com>
2023-09-04 20:50:11 -0700

    add icon changes

--- frontend/src/components/OwnedGamesButton/OwnedGamesButton.jsx
+++ frontend/src/components/OwnedGamesButton/OwnedGamesButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import LibraryButton from "../../assets/images/add-to-library.svg";
 import FilledLibraryButton from "../../assets/images/filled-library.svg";
@@ -10,36 +11,41 @@ const OwnedGamesButton = ({ gameData }) => {
 	const dispatch = useDispatch();
 	const currentUser = useSelector(state => state.user);
 	const ownedGames = useSelector(state => state.ownedGames);
-	const wishlist = useSelector(state => state.wishlist) ;
-
-	useEffect(() => {
-
-	}, [gameData, ownedGames, wishlist, currentUser])
-
-	const handleGamesList = (e) => {
-		e.preventDefault();
-		// Remove game from ownedGames
-		if (gameData.gameId.toString() in ownedGames) {
-			dispatch(deleteOwnedGamesItem(currentUser.id, gameData.gameId));
-		} else {
-			// If game is in wishlist, remove, then add to ownedGames
-			if(gameData.gameId.toString() in wishlist) {
-				dispatch(deleteWishlistItem(currentUser.id, gameData.gameId));
-			}
-			dispatch(addOwnedGamesItem(currentUser.id, gameData));
-		}
-	}
-
-	const iconButton = () => {
+	const wishlist = useSelector(state => state.wishlist);
+	const [icon, setIcon] = useState(() => {
 		if (gameData.gameId.toString() in ownedGames) {
 			return FilledLibraryButton;
 		} else {
 			return LibraryButton;
 		}
+	});
+
+	useEffect(() => {
+		if (gameData.gameId.toString() in ownedGames) {
+			setIcon(FilledLibraryButton);
+		} else {
+			setIcon(LibraryButton);
+		}
+	}, [dispatch])
+
+	const handleGamesList = (e) => {
+		e.preventDefault();
+		// Remove game from ownedGames
+		if (gameData.gameId.toString() in ownedGames) {
+			dispatch(deleteOwnedGamesItem(currentUser.id, gameData.gameId))
+			setIcon(LibraryButton)
+		} else {
+			// If game is in wishlist, remove, then add to ownedGames
+			if(gameData.gameId.toString() in wishlist) {
+				dispatch(deleteWishlistItem(currentUser.id, gameData.gameId));
+			}
+			dispatch(addOwnedGamesItem(currentUser.id, gameData))
+			setIcon(FilledLibraryButton)
+		}
 	}
 
 	return (
-		<img src={iconButton()} alt='' className='owned-game-button' onClick={handleGamesList}/>
+		<img src={icon} alt='' className='owned-game-button' onClick={handleGamesList}/>
 	)
 }
 
