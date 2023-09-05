908279c0bead8168e839b8c1ca9b1ced5389541b
Weyman Leung <leungweyman@gmail.com>
2023-09-04 22:07:30 -0700

    added the changing icon for wishlist

--- frontend/src/components/WishlistButton/WishlistButton.jsx
+++ frontend/src/components/WishlistButton/WishlistButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import wishlistButton from "../../assets/images/wishlist.svg";
 import FilledWishlistButton from "../../assets/images/filled-heart.svg";
@@ -11,34 +12,40 @@ const WishlistButton = ({ gameData }) => {
 	const currentUser = useSelector(state => state.user);
 	const wishlist = useSelector(state => state.wishlist);
 	const ownedGames = useSelector(state => state.ownedGames)
-	
-	useEffect(() => {
+	const [icon, setIcon] = useState(() => {
+		if (gameData.gameId.toString() in wishlist) {
+			return FilledWishlistButton;
+		} else {
+			return wishlistButton;
+		}
+	});
 
-	}, [gameData, wishlist, ownedGames, currentUser])
+	useEffect(() => {
+		if (gameData.gameId.toString() in wishlist) {
+			setIcon(FilledWishlistButton);
+		} else {
+			setIcon(wishlistButton);
+		}
+	}, [dispatch])
 
 	const handleWishlist = (e) => {
 		e.preventDefault();
 		// Remove game from wishlist
 		if (gameData.gameId.toString() in wishlist) {
 			dispatch(deleteWishlistItem(currentUser.id, gameData.gameId));
+			setIcon(wishlistButton)
 		} else {
 			// If game is in ownedGames, remove, then add to wishlist
 			if(gameData.gameId.toString() in ownedGames) {
 				dispatch(deleteOwnedGamesItem(currentUser.id, gameData.gameId));
 			}
 			dispatch(addWishlistItem(currentUser.id, gameData));
+			setIcon(FilledWishlistButton)
 		}
 	}
 
-	const iconButton = () => {
-		if (gameData.gameId.toString() in wishlist) {
-			return FilledWishlistButton;
-		} else {
-			return wishlistButton;
-		}
-	}
 	return (
-		<img src={iconButton()} alt='' className="wishlist-button" onClick={handleWishlist}/>
+		<img src={icon} alt='' className="wishlist-button" onClick={handleWishlist}/>
 	)
 }
 
