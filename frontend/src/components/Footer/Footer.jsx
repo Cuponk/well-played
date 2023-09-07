4b3e86bd82059ad841d23648aeda83f6d62a439e
Weyman Leung <leungweyman@gmail.com>
2023-09-07 15:03:14 -0700

    add social links to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import "./Footer.css";
+import github from "../../assets/images/github.png";
+import linkedin from "../../assets/images/linkedin.png";
 
 function Footer() {
   return (
@@ -6,18 +8,36 @@ function Footer() {
       <div className="authors">
         <div className="author-container">
           <h3>Justin Aitken</h3>
-          <p>Github</p>
-          <p>LinkedIn</p>
+		  <div className="socials-container">
+		  	<a href="https://github.com/Cuponk" target="_blank">
+				<img src={github} className="social-links"/>
+			</a>
+		  	<a href="https://www.linkedin.com/in/justin-aitken-bb9272212/" target="_blank">
+				<img src={linkedin} className="social-links"/>
+			</a>
+		  </div>
         </div>
         <div className="author-container">
           <h3>Weyman Leung</h3>
-          <p>Github</p>
-          <p>LinkedIn</p>
+		  <div className="socials-container">
+		  <a href="https://github.com/wleung4" target="_blank">
+				<img src={github} className="social-links"/>
+			</a>
+		  	<a href="https://www.linkedin.com/in/weyman-leung/" target="_blank">
+				<img src={linkedin} className="social-links"/>
+			</a>
+		  </div>
         </div>
         <div className="author-container">
           <h3>Julio Uribe</h3>
-          <p>Github</p>
-          <p>LinkedIn</p>
+		  <div className="socials-container">
+		  	<a href="https://github.com/juliouribe" target="_blank">
+				<img src={github} className="social-links"/>
+			</a>
+		  	<a href="https://www.linkedin.com/in/julio-uribe-a15736b5/" target="_blank">
+				<img src={linkedin} className="social-links"/>
+			</a>
+		  </div>
         </div>
       </div>
       <p className="copy-right">Copyright &copy;  2023 Well Played &reg;</p>
